a5131614d6f8fc04ae4705bdb1b55811bfbeeb3a
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-07 00:14:00 +0300

    registering bootstrap in the init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from app.config import DevConfig
+from flask_bootstrap import Bootstrap
 
-# Initializing application
-app = Flask(__name__)
+# initializing the application
+# instance_relative_config allows us to connect with the instance folder holding the api_key
+app = Flask(__name__,instance_relative_config=True)
+
 
 # setting up configuration
 app.config.from_object(DevConfig)
 # connect the app with the config holding the api_key
 app.config.from_pyfile("config.py")
+
+# initialising Flask Extensions
+# We then initialize the Bootstrap class by passing in the app instance. This is how most extensions are initialized. We can now use bootstrap in our application.
+bootstrap = Bootstrap(app)
 
 from app import views
