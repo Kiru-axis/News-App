470eda1a60b550ce17c91bc13f2164998d7f56e6
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-07 02:21:19 +0300

    global function to  access to app config objects

--- app/request.py
+++ app/request.py
@@ -2,13 +2,19 @@ from app import app
 import urllib.request,json
 from .models import news
 
-
 News = news.News
 Sources = news.Sources
-# Getting api_key
-api_key = app.config["NEWS_API_KEY"]
-# Getting base url
-base_url = app.config["NEWS_API_BASE_URL"]
+
+
+# getting api key
+api_key = None
+# getting the news base url
+base_url = None
+
+def configure_request(app):
+    global api_key, base_url
+    api_key = app.config["NEWS_API_KEY"]
+    base_url = app.config["NEWS_API_BASE_URL"]
 
 # __________________________News class ________________________
 
