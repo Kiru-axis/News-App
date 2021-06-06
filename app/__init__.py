f48b7ffc32b13d8bb0b6858323ce1523a53044a8
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-06 23:49:06 +0300

    setting up configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from app.config import DevConfig
 
 # Initializing application
 app = Flask(__name__)
 
+# setting up configuration
+app.config.from_object(DevConfig)
+# connect the app with the config holding the api_key
+app.config.from_pyfile("config.py")
 
 from app import views
