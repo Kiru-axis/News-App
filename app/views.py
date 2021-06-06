e43a924fcb6a48c73b71932ed841be3149e5a0b6
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-07 00:04:51 +0300

    creating the source news route and function

--- app/views.py
+++ app/views.py
@@ -3,9 +3,22 @@ from app import app
 
 # Views
 @app.route('/')
+# view function
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+
+# Sources function and routes
+@app.route('/sources')
+def sources():
+    """
+    View function to display sources of the news feed
+    """
+    source = sources_news()
+    title = f"{sources} news"
+    return render_template("sources.html", source = source)
+
