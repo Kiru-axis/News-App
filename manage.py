65fd3641ad58742ead961e1b22d607e5ff97d574
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-07 02:53:35 +0300

    fixing no module flask._compact error

--- manage.py
+++ manage.py
@@ -1,12 +1,10 @@
 from app import create_app
-from flask_script import Manager, Server
+from flask_script import Manager,Server
 
 # Creating app instance
-app = create_app("development")
+app = create_app('development')
 
 manager = Manager(app)
-
-manager.add_command("server", Server)
-
-if __name__ == "__main__":
+manager.add_command('server',Server)
+if __name__ == '__main__':
     manager.run()
